19f7242b611073435d52f8a61781d4142ecb5408
lipeng <marsapolo@163.com>
2018-08-06 15:01:09 +0800

    marsapolo test

--- meta.js
+++ meta.js
@@ -44,7 +44,7 @@ module.exports = {
       type: 'string',
       required: false,
       message: 'Project description',
-      default: 'A Vue.js project',
+      default: 'marsapolo test',
     },
     author: {
       when: 'isNotTest',
@@ -157,20 +157,20 @@ module.exports = {
       ],
     },
   },
-  filters: {
-    '.eslintrc.js': 'lint',
-    '.eslintignore': 'lint',
-    'config/test.env.js': 'unit || e2e',
-    'build/webpack.test.conf.js': "unit && runner === 'karma'",
-    'test/unit/**/*': 'unit',
-    'test/unit/index.js': "unit && runner === 'karma'",
-    'test/unit/jest.conf.js': "unit && runner === 'jest'",
-    'test/unit/karma.conf.js': "unit && runner === 'karma'",
-    'test/unit/specs/index.js': "unit && runner === 'karma'",
-    'test/unit/setup.js': "unit && runner === 'jest'",
-    'test/e2e/**/*': 'e2e',
-    'src/router/**/*': 'router',
-  },
+  // filters: {
+  //   '.eslintrc.js': 'lint',
+  //   '.eslintignore': 'lint',
+  //   'config/test.env.js': 'unit || e2e',
+  //   'build/webpack.test.conf.js': "unit && runner === 'karma'",
+  //   'test/unit/**/*': 'unit',
+  //   'test/unit/index.js': "unit && runner === 'karma'",
+  //   'test/unit/jest.conf.js': "unit && runner === 'jest'",
+  //   'test/unit/karma.conf.js': "unit && runner === 'karma'",
+  //   'test/unit/specs/index.js': "unit && runner === 'karma'",
+  //   'test/unit/setup.js': "unit && runner === 'jest'",
+  //   'test/e2e/**/*': 'e2e',
+  //   'src/router/**/*': 'router',
+  // },
   complete: function(data, { chalk }) {
     const green = chalk.green
 
