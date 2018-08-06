6d04ed9f4c7662e565a5b4486f6e8799668b89e5
lipeng <marsapolo@163.com>
2018-08-06 14:04:17 +0800

    test

--- template/src/main.js
+++ template/src/main.js
@@ -1,26 +1,45 @@
-{{#if_eq build "standalone"}}
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-{{/if_eq}}
 import Vue from 'vue'
+
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import '@/styles/index.scss' // global css
+
 import App from './App'
-{{#router}}
 import router from './router'
-{{/router}}
+import store from './store'
+
+import i18n from './lang' // Internationalization
+import './icons' // icon
+import './errorLog' // error log
+import './permission' // permission control
+// import './mock' // simulation data
+
+// 引入moment日期格式化组件
+import moment from 'moment'
+Vue.prototype.$moment = moment
+
+import * as filters from './filters' // global filters
+
+Vue.use(Element, {
+  size: 'small', // set element-ui default size
+  i18n: (key, value) => i18n.t(key, value)
+})
+
+// register global utility filters.
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  {{#router}}
   router,
-  {{/router}}
-  {{#if_eq build "runtime"}}
-  render: h => h(App)
-  {{/if_eq}}
-  {{#if_eq build "standalone"}}
-  components: { App },
-  template: '<App/>'
-  {{/if_eq}}
+  store,
+  i18n,
+  template: '<App/>',
+  components: { App }
 })
